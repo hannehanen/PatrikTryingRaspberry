72ce31d75c18ee68a9b731485606a9a56435c064
Patrik Hansson <patrikhansson17@hotmail.com>
2019-04-21 07:01:53 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+
+const socket = io('http://192.168.1.8:8080');
 
 class App extends Component {
+
+  componentDidMount() {
+   
+  }
+
+  changeThings = (ev) =>{
+    socket.emit("light",Number(1))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <p><input onChange={this.changeThings} type="checkbox" id="light"/>Box..</p>
       </div>
     );
   }
